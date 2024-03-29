6087123a4bc34cd13f047f4b32a123d089294b3c
Shinzuu <not.shinzuu@gmail.com>
2024-03-29 21:04:53 +0600

    test 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let num = string.length;
+    let nweString = "";
+    for(let i = num - 1; i >= 0; i--){
+        nweString += string[i];
+    }
 
+    return nweString;
 };
 
 // Do not edit below this line
