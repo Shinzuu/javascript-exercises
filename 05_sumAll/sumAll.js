e04984490c2c8b7727d5346be0af5248e041c808
Shinzuu <not.shinzuu@gmail.com>
2024-03-30 01:43:57 +0600

    test 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    if (typeof a !== 'number' || typeof b !== 'number' || a < 0 || b < 0) {
+        return "ERROR";
+    } else {
+        if (a > b) {
+            let temp = b;
+            b = a;
+            a = temp;
+        }
+        let sum = ((b - a + 1) / 2) * (a + b);
+        return sum;
+    }
 };
 
 // Do not edit below this line
