e2be99fac572b30acdf66e49576e112797070ca0
Shinzuu <not.shinzuu@gmail.com>
2024-04-14 02:46:27 +0600

    09 comp

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const cleanedString = string.toLowerCase().split('').filter(char => /[a-z0-9]/.test(char)).join('');
+    const reversedString = cleanedString.split('').reverse().join('');
+    if(cleanedString === reversedString) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
