ce47e2eb86849a021dc9df0ad2289f014d04736f
Shinzuu <not.shinzuu@gmail.com>
2024-04-14 03:22:24 +0600

    ok

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,30 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    const currentYear = new Date().getFullYear();
 
+    let oldest = {
+        name: "",
+        age: -Infinity // Initialize age with negative infinity to ensure any person's age will be greater
+    };
+
+    for (let i = 0; i < array.length; i++) {
+        const person = array[i];
+
+        // If year of death is undefined, set it to the current year
+        if (person.yearOfDeath === undefined) {
+            person.yearOfDeath = currentYear;
+        }
+
+        // Calculate the age of the person
+        const age = person.yearOfDeath - person.yearOfBirth;
+
+        // Update the oldest object if the current person is older
+        if (age > oldest.age) {
+            oldest.name = person.name;
+            oldest.age = age;
+        }
+    }
+
+    return oldest; // Return the object containing the name and age of the oldest person
 };
 
-// Do not edit below this line
 module.exports = findTheOldest;
