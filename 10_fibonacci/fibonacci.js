628b193dc5c80ff369e88d19e195364dd550d877
Shinzuu <not.shinzuu@gmail.com>
2024-04-14 02:55:38 +0600

    10 fib

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(a) {
+    if (a < 0) return "OOPS";
+    if (a == 0) return 0;
+    let firstPrev = 1;
+    let secondPrev = 0;
+    for (let i = 2; i <= a; i++) {
+        let current = firstPrev + secondPrev;
+        secondPrev = firstPrev;
+        firstPrev = current;
+    }
+    return firstPrev;
 };
 
 // Do not edit below this line
